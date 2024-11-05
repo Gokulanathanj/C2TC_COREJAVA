044e5269902c823968524857ea245dc85edd756a
gokul <gokul@GOKULANATHAN>
2024-11-05 08:15:10 +0530

    fourth

---
+++ C2TC_GOKUL(CSE)/src/com/tns/ifet/day2/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day2;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new
+		OopsConceptDemo();
+		obj.setSerialNum(28);
+		obj.setName("Dhanush");
+		obj.setAge(21);
+		System.out.println(obj);
+		}
+}
