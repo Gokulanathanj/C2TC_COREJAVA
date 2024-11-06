1316b6a22ee7e69729bd5dd0f6b0774833fc8372
gokul <gokul@GOKULANATHAN>
2024-11-06 11:23:04 +0530

    recommit

--- C2TC_GOKUL(CSE)/src/com/tns/ifet/day2/EncapsulationDemo.java
+++ C2TC_GOKUL(CSE)/src/com/tns/ifet/day2/EncapsulationDemo.java
@@ -4,8 +4,8 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		OopsConceptDemo obj = new
 		OopsConceptDemo();
-		obj.setSerialNum(28);
-		obj.setName("Dhanush");
+		obj.setSerialNum(39);
+		obj.setName("Gokul");
 		obj.setAge(21);
 		System.out.println(obj);
 		}
